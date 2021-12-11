f672aa566237beb55498c94fe5b3833d37ff4f83
Jung kyu Lee <48317269+ljk9410@users.noreply.github.com>
2021-12-11 22:41:01 +0900

    props UI 패턴

--- 코딩애플/blog/src/App.js
+++ 코딩애플/blog/src/App.js
@@ -5,14 +5,22 @@ import './App.css';
 function App() {
 
   let [글제목, 글제목변경] = useState(['상도 맛집', '흑석 맛집', '강남 맛집']);
-  let [따봉, 따봉변경] = useState([0, 1, 2]);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, modalChange] = useState(false);
-  
+  let [누른제목, 누른제목변경] = useState(0);
+
   function 제목바꾸기() {
     let newArray = [...글제목];
 
     newArray[0] = '상도 고기 맛집';
     글제목변경(newArray);
+  }
+
+  function 따봉바꾸기(i) {
+    let newArray = [...따봉];
+
+    newArray[i]++;
+    따봉변경(newArray);
   }
 
   return (
@@ -26,7 +34,7 @@ function App() {
         글제목.map((글, i) => {
           return (
             <div className="list">
-                <h3> { 글 } </h3>
+                <h3 onClick={ ()=> { 누른제목변경(i) }}> { 글 } <span onClick={ ()=> { 따봉바꾸기(i) } }>👍</span> { 따봉[i] } </h3>
                 <p>2월 17일 발행</p>
                 <hr/>
             </div>  
@@ -37,7 +45,7 @@ function App() {
       <button onClick={ () => { modalChange(!modal) } }>modal</button>
       {
         modal === true
-        ? <Modal 글제목 = { 글제목 } ></Modal>
+        ? <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
         : null
       }
     </div>
@@ -47,7 +55,7 @@ function App() {
 function Modal(props) {
   return (
   <div className="modal">
-    <h2>{ props.글제목[0] }</h2>
+    <h2>{ props.글제목[props.누른제목] }</h2>
     <p>날짜</p>
     <p>상세내용</p>
   </div>
