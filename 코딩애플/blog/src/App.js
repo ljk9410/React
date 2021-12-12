55a507d8f40b53b5f7ebee4d9ef89bea946ce288
Jung kyu Lee <48317269+ljk9410@users.noreply.github.com>
2021-12-12 13:38:00 +0900

    react에서 input 데이터 다루기

--- 코딩애플/blog/src/App.js
+++ 코딩애플/blog/src/App.js
@@ -8,6 +8,9 @@ function App() {
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, modalChange] = useState(false);
   let [누른제목, 누른제목변경] = useState(0);
+  let [입력값, 입력값변경] = useState('');
+
+
 
   function 제목바꾸기() {
     let newArray = [...글제목];
@@ -23,6 +26,8 @@ function App() {
     따봉변경(newArray);
   }
 
+
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -30,10 +35,11 @@ function App() {
       </div>
       <button onClick={ 제목바꾸기 }>버튼</button>
 
+
       {
         글제목.map((글, i) => {
           return (
-            <div className="list">
+            <div className="list" key={i}>
                 <h3 onClick={ ()=> { 누른제목변경(i) }}> { 글 } <span onClick={ ()=> { 따봉바꾸기(i) } }>👍</span> { 따봉[i] } </h3>
                 <p>2월 17일 발행</p>
                 <hr/>
@@ -41,6 +47,20 @@ function App() {
           ) 
         })
       }
+
+
+      <div className="publish">
+        <input onChange={ (e)=>{ 입력값변경(e.target.value) } }/>
+        <button onClick={ ()=> { 
+          let newArray = [...글제목]
+
+          newArray.unshift(입력값)
+          글제목변경(newArray)
+         } }>저장</button>
+      </div>
+
+      <Profile />
+
 
       <button onClick={ () => { modalChange(!modal) } }>modal</button>
       {
@@ -62,6 +82,25 @@ function Modal(props) {
   )
 }
 
+class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = { name:'lee', age:28 }
+  }
 
+  changeName() {
+    this.setState( {name:'Park'} )
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>클래스</h3>
+        <p>{ this.state.name }</p>
+        <button onClick={ this.changeName.bind(this) }>변경</button>
+      </div>
+    )
+  }
+}
 
 export default App;
